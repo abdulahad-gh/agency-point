769a0247bc2b1747e0a7f87420a888f1febcfc2a
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-07-04 17:29:05 +0600

    agency-point project setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-red-500'>this is first section of landing page</h1>
+
+        </div>
+    );
+};
+
+export default Home;
