747f26c3998d290a63ff7bd19d4438dc4db6873b
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-07-05 20:54:56 +0600

    completed ourWorks section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Shared/Banner';
+import OurWorks from './Shared/OurWorks';
 import ProvideServices from './Shared/ProvideServices';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner />
             <ProvideServices />
+            <OurWorks />
 
         </div>
     );
